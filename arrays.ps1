f7ed03b2eab1e9119a24d2cf9149bc559413a2e5
hrthk <hrthksly@gmail.com>
2026-07-22 16:10:33 +0530

    add array examples

--- arrays.ps1
+++ arrays.ps1
@@ -9,3 +9,33 @@ foreach($server in $servers)
 {
     Test-Connection $server -Count 1
 }
+
+$servers = @(
+    "localhost",
+    "127.0.0.1"
+)
+
+foreach ($server in $servers)
+{
+    Test-Connection $server -Count 1
+}
+
+$servers = @(
+    "192.168.1.10",
+    "192.168.1.11"
+)
+foreach ($server in $servers)
+{
+    Test-Connection $server -Count 1
+}
+
+$computers = @(
+"PC01",
+"PC02",
+"PC03"
+)
+
+foreach($computer in $computers)
+{
+    Invoke-Command -ComputerName $computer
+}
